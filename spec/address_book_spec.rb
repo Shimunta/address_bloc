c8779f2aa2d1b64417bb6752198039d856bcc244
Daniel Lisa <danlisa89@gmail.com>
2018-05-13 17:29:11 +0000

    assignment complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -29,4 +29,11 @@ RSpec.describe AddressBook do
             expect(new_entry.email).to eq('augusta.king@lovelace.com')
         end
     end
+    describe '#remove_entry' do
+        it "removes only one entry to the address book" do 
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            expect(book.entries.size).to eq(0)
+        end
+    end
 end
